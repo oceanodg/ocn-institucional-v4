9f9207edb57d7ec6000c046694eaba98ed766337
Tiago Petrucci <tiagopetrucci@gmail.com>
2026-04-03 14:51:35 -0300

    feat: enhance Sheet component with title and description accessibility using VisuallyHidden

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as SheetPrimitive from "@radix-ui/react-dialog";
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
 import { XIcon } from "lucide-react";
 
 import { cn } from "~/lib/utils";
@@ -46,9 +47,11 @@ function SheetContent({
   className,
   children,
   side = "right",
+  title,
   ...props
 }: React.ComponentProps<typeof SheetPrimitive.Content> & {
   side?: "top" | "right" | "bottom" | "left";
+  title?: string;
 }) {
   return (
     <SheetPortal>
@@ -69,6 +72,12 @@ function SheetContent({
         )}
         {...props}
       >
+        <VisuallyHidden>
+          <SheetPrimitive.Title>{title ?? "Menu"}</SheetPrimitive.Title>
+          <SheetPrimitive.Description>
+            {title ?? "Menu"}
+          </SheetPrimitive.Description>
+        </VisuallyHidden>
         {children}
         <SheetPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-secondary absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none">
           <XIcon className="size-4" />
