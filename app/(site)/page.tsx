f14a9462cdc689b372c79ee7ee8b4a0986accf71
Tiago Petrucci <tiagopetrucci@gmail.com>
2026-04-08 07:16:57 -0300

    style: enhance welcome page text styling for improved readability and emphasis

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -38,9 +38,13 @@ export default function WelcomePage() {
     <section className="relative backdrop-blur-sm">
       <HeroContainer className="pb-2 sm:pb-0">
         <div className="flex flex-col gap-2">
-          <H1 className="font-semibold text-left flex flex-col gap-0">
-            <span className="text-gray-500 italic">Bem-vindo à</span>
-            <span className="text-gray-700">Igreja Oceano da Graça</span>
+          <H1 className="text-left flex flex-col gap-0 sm:gap-0">
+            <span className="font-semibold text-gray-500 italic">
+              Bem-vindo à
+            </span>
+            <span className="font-bold text-gray-700">
+              Igreja Oceano da Graça
+            </span>
           </H1>
           <P className="mt-0">
             Aqui você encontra um espaço preparado para viver a fé. Venha ser
