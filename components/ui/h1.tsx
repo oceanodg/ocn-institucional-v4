82d2d3b48aab0fb60dc9e3869ac9234cf1a7bad8
Tiago Petrucci <tiagopetrucci@gmail.com>
2026-04-09 07:25:11 -0300

    style: adjust H1 component text size for improved responsiveness

--- components/ui/h1.tsx
+++ components/ui/h1.tsx
@@ -11,7 +11,7 @@ export function H1({
   return (
     <h1
       className={cn(
-        "scroll-m-20 text-center text-2xl md:text-3xl lg:text-4xl font-bold tracking-tight text-balance text-gray-700",
+        "scroll-m-20 text-center text-3xl sm:text-4xl font-bold tracking-tight text-balance text-gray-700",
         lora.className,
         className
       )}
