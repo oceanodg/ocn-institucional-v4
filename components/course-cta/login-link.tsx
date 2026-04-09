c5dc03b5056361b92766433e9df4081d4aba97e8
Tiago Petrucci <tiagopetrucci@gmail.com>
2026-04-09 16:52:02 -0300

    feat: enhance LoginLink component to accept custom className for improved styling flexibility

--- components/course-cta/login-link.tsx
+++ components/course-cta/login-link.tsx
@@ -1,15 +1,19 @@
 import { Container } from "~/components/container";
 import { P } from "~/components/ui";
+import { cn } from "~/lib/utils";
 
 type LoginLinkProps = {
   loginUrl: string;
+  className?: string;
 };
 
 export function LoginLink(props: LoginLinkProps) {
-  const { loginUrl } = props;
+  const { loginUrl, className } = props;
 
   return (
-    <Container className="w-full sm:w-96 p-4 sm:p-6 gap-3 lg:gap-3">
+    <Container
+      className={cn("w-full sm:w-96 p-4 sm:p-6 gap-3 lg:gap-3", className)}
+    >
       <P className="mt-0 sm:mt-0 mx-auto sm:mx-0">
         Já é cadastrado(a)?{" "}
         <a
