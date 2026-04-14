8b59a5c863e6bf73af8dfc2188a09ada66592fc6
Tiago Petrucci <tiagopetrucci@gmail.com>
2026-04-14 09:46:56 -0300

    feat: add new mind map link for Mapa Mental to enhance educational resources in Lucas course materials

--- app/(site)/oceano-academy/materiais-didaticos/lucas/page.tsx
+++ app/(site)/oceano-academy/materiais-didaticos/lucas/page.tsx
@@ -1,4 +1,4 @@
-import { FileText, Hash } from "lucide-react";
+import { FileText, Hash, Workflow } from "lucide-react";
 import { HeroContainer } from "~/components/hero";
 import { AllTeachingMaterialsBackButton } from "~/components/back-buttons/all-teaching-materials";
 import { H1, H2, H3, P, Separator } from "~/components/ui";
@@ -52,6 +52,19 @@ export default function CursoLucas() {
                   <LinkSmall href="/oceano-academy/materiais-didaticos/lucas.md">
                     <Hash className="size-4" />
                     MD
+                  </LinkSmall>
+                </TableCellLinksContainer>
+              </TableCell>
+            </TableRow>
+            <TableRow className="hover:bg-transparent">
+              <TableCell className="border font-semibold">
+                Mapa Mental
+              </TableCell>
+              <TableCell>
+                <TableCellLinksContainer>
+                  <LinkSmall href="https://whimsical.com/ocn7/ocn-escola-biblica-GgrjZLEXpFtuzvGrxYkM7b">
+                    <Workflow className="size-4" />
+                    Mapa Mental
                   </LinkSmall>
                 </TableCellLinksContainer>
               </TableCell>
