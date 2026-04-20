691f6c1b9165a7275c021d3abf40693c56e156be
Tiago Petrucci <tiagopetrucci@gmail.com>
2026-04-19 22:40:14 -0300

    feat: add recommended video, mind map, and course links for João e Judas materials, enhancing educational resources for students

--- app/(site)/oceano-academy/materiais-didaticos/cartas-joao-e-judas/page.tsx
+++ app/(site)/oceano-academy/materiais-didaticos/cartas-joao-e-judas/page.tsx
@@ -1,4 +1,10 @@
-import { FileText, Hash } from "lucide-react";
+import {
+  FileText,
+  GraduationCap,
+  Hash,
+  SquarePlay,
+  Workflow,
+} from "lucide-react";
 import { HeroContainer } from "~/components/hero";
 import { AllTeachingMaterialsBackButton } from "~/components/back-buttons/all-teaching-materials";
 import { H1, H2, H3, P, Separator } from "~/components/ui";
@@ -53,6 +59,49 @@ export default function CursoCartasJoaoEJudas() {
                   <LinkSmall href="/oceano-academy/materiais-didaticos/cartas-joao-e-judas.md">
                     <Hash className="size-4" />
                     MD
+                  </LinkSmall>
+                </TableCellLinksContainer>
+              </TableCell>
+            </TableRow>
+            <TableRow className="hover:bg-transparent">
+              <TableCell className="border font-semibold">
+                Mapa Mental
+              </TableCell>
+              <TableCell>
+                <TableCellLinksContainer>
+                  <LinkSmall href="https://whimsical.com/ocn7/ocn-escola-biblica-GgrjZLEXpFtuzvGrxYkM7b">
+                    <Workflow className="size-4" />
+                    Mapa Mental
+                  </LinkSmall>
+                </TableCellLinksContainer>
+              </TableCell>
+            </TableRow>
+            <TableRow className="hover:bg-transparent">
+              <TableCell className="border font-semibold">
+                Vídeos Recomendados
+              </TableCell>
+              <TableCell>
+                <TableCellLinksContainer>
+                  <LinkSmall href="https://www.youtube.com/watch?v=vKZ-RGmZs-8">
+                    <SquarePlay className="size-4" />
+                    1-3 João
+                  </LinkSmall>
+                  <LinkSmall href="https://www.youtube.com/watch?v=Ep975kf8vXE">
+                    <SquarePlay className="size-4" />
+                    Judas
+                  </LinkSmall>
+                </TableCellLinksContainer>
+              </TableCell>
+            </TableRow>
+            <TableRow className="hover:bg-transparent">
+              <TableCell className="border font-semibold">
+                Curso Recomendado
+              </TableCell>
+              <TableCell>
+                <TableCellLinksContainer>
+                  <LinkSmall href="/oceano-academy/cursos/fundamentos">
+                    <GraduationCap className="size-4" />
+                    Fundamentos
                   </LinkSmall>
                 </TableCellLinksContainer>
               </TableCell>
