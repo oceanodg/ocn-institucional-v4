5a59c5dfb15b97544b4c98fb3c4dce754c1a6954
Tiago Petrucci <tiagopetrucci@gmail.com>
2026-04-27 10:57:39 -0300

    feat: add study plan link for Filipenses materials, enhancing educational resources for students

--- app/(site)/oceano-academy/materiais-didaticos/filipenses/page.tsx
+++ app/(site)/oceano-academy/materiais-didaticos/filipenses/page.tsx
@@ -64,6 +64,19 @@ export default function CursoFilipenses() {
             </TableRow>
             <TableRow className="hover:bg-transparent">
               <TableCell className="border font-semibold">
+                Plano de Estudo
+              </TableCell>
+              <TableCell>
+                <TableCellLinksContainer>
+                  <LinkSmall href="https://drive.google.com/file/d/1Buwz0bieUJMdDWK4FdbPqt7xOP3GTbAh/view?usp=sharing">
+                    <FileText className="size-4" />
+                    Plano de Estudo
+                  </LinkSmall>
+                </TableCellLinksContainer>
+              </TableCell>
+            </TableRow>
+            <TableRow className="hover:bg-transparent">
+              <TableCell className="border font-semibold">
                 Mapa Mental
               </TableCell>
               <TableCell>
