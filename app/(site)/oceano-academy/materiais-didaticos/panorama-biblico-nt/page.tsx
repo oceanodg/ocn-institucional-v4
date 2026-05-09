05f51d98bbcc67761cd350cf7cad5f4efb29531d
Tiago Petrucci <tiagopetrucci@gmail.com>
2026-05-09 20:45:17 -0300

    feat: implement Panorama Bíblico do Novo Testamento page with educational materials and summary for enhanced learning experience

---
+++ app/(site)/oceano-academy/materiais-didaticos/panorama-biblico-nt/page.tsx
@@ -0,0 +1,101 @@
+import {
+  FileImage,
+  FileText,
+  GraduationCap,
+  Hash,
+  SquarePlay,
+  Workflow,
+} from "lucide-react";
+import { HeroContainer } from "~/components/hero";
+import { AllTeachingMaterialsBackButton } from "~/components/back-buttons/all-teaching-materials";
+import { H1, H2, H3, P, Separator } from "~/components/ui";
+import { Container } from "~/components/container";
+import { UL } from "~/components/ui/ul";
+import { SummaryLi } from "~/components/summary-li";
+import { Table, TableCell, TableHeader, TableRow } from "~/components/ui/table";
+import { LinkSmall } from "~/components/ui/link-small";
+import { TableCellLinksContainer } from "~/components/ui/table-cell-links-container";
+
+export const metadata = {
+  alternates: {
+    types: {
+      "text/markdown":
+        "/oceano-academy/materiais-didaticos/panorama-biblico-nt.md",
+    },
+  },
+};
+
+export default function PanoramaBiblicoNT() {
+  return (
+    <section className="relative backdrop-blur-sm">
+      <AllTeachingMaterialsBackButton tab="novo" />
+
+      <HeroContainer className="pb-2 sm:pb-0">
+        <div className="flex flex-col gap-2">
+          <H1 className="text-left">Panorama Bíblico do Novo Testamento</H1>
+          <P className="mt-0">
+            Uma visão geral da história bíblica do Novo Testamento, uma
+            compilação de livros e cartas escritas após o nascimento de Jesus.
+          </P>
+        </div>
+      </HeroContainer>
+
+      <Separator className="my-8" />
+
+      <Container className="mb-10 sm:mb-16">
+        <H2 id="materials-didactic">Materiais de Apoio</H2>
+
+        <Table>
+          <TableHeader>
+            <TableRow className="hover:bg-transparent">
+              <TableCell className="border font-semibold w-[110px] sm:w-[160px]">
+                Apostila
+              </TableCell>
+              <TableCell>
+                <TableCellLinksContainer>
+                  <LinkSmall href="https://drive.google.com/file/d/1L2_zinjGqSVyUqdpf4aIpipoeFvEgija/view?usp=sharing">
+                    <FileText className="size-4" />
+                    PDF
+                  </LinkSmall>
+                  <LinkSmall href="/oceano-academy/materiais-didaticos/panorama-biblico-nt.md">
+                    <Hash className="size-4" />
+                    MD
+                  </LinkSmall>
+                </TableCellLinksContainer>
+              </TableCell>
+            </TableRow>
+            <TableRow className="hover:bg-transparent">
+              <TableCell className="border font-semibold">
+                Curso Recomendado
+              </TableCell>
+              <TableCell>
+                <TableCellLinksContainer>
+                  <LinkSmall href="/oceano-academy/cursos/fundamentos">
+                    <GraduationCap className="size-4" />
+                    Fundamentos
+                  </LinkSmall>
+                </TableCellLinksContainer>
+              </TableCell>
+            </TableRow>
+          </TableHeader>
+        </Table>
+      </Container>
+
+      <Separator className="my-8" />
+
+      <Container className="mb-10 sm:mb-16">
+        <H2>Sumário</H2>
+        <UL className="mt-0 space-y-2 sm:space-y-2">
+          <SummaryLi>
+            <a href="#lesson-1">Lição 1 – Foi Assim Que Tudo Começou</a>
+          </SummaryLi>
+          <SummaryLi subList>
+            <a href="#lesson-1-objetivo-geral">Objetivo Geral</a>
+          </SummaryLi>
+        </UL>
+      </Container>
+
+      <Separator className="my-8" />
+    </section>
+  );
+}
