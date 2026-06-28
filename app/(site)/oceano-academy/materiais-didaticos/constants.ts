9190f8981891967ef30724aa4c39a32ada5bf882
Tiago Petrucci <tiagopetrucci@gmail.com>
2026-06-28 13:52:06 -0300

    feat: add "parabolas" to validTabs in constants for expanded tab options

--- app/(site)/oceano-academy/materiais-didaticos/constants.ts
+++ app/(site)/oceano-academy/materiais-didaticos/constants.ts
@@ -1,2 +1,7 @@
-export const validTabs = ["antigo", "novo", "personagens"] as const;
+export const validTabs = [
+  "antigo",
+  "novo",
+  "personagens",
+  "parabolas",
+] as const;
 export type ValidTab = (typeof validTabs)[number];
