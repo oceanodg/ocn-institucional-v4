6336db5fd828392836a07c0e2d9dc8517c706d41
Tiago Petrucci <tiagopetrucci@gmail.com>
2026-06-30 16:58:37 -0300

    feat: create PanoramaBiblicoAT page with educational materials and resources for Genesis

---
+++ app/(site)/oceano-academy/materiais-didaticos/genesis/page.tsx
@@ -0,0 +1,85 @@
+import {
+  CircleUserRound,
+  ContactRound,
+  FileImage,
+  FileText,
+  GraduationCap,
+  Hash,
+  SquarePlay,
+  User,
+  UserRound,
+  UserStar,
+  Wallpaper,
+  Workflow,
+} from "lucide-react";
+import { HeroContainer } from "~/components/hero";
+import { AllTeachingMaterialsBackButton } from "~/components/back-buttons/all-teaching-materials";
+import { H1, H2, H3, P, Separator } from "~/components/ui";
+import { Container } from "~/components/container";
+import { UL } from "~/components/ui/ul";
+import { SummaryLi } from "~/components/summary-li";
+import { Table, TableCell, TableHeader, TableRow } from "~/components/ui/table";
+import { LinkSmall } from "~/components/ui/link-small";
+import { TableCellLinksContainer } from "~/components/ui/table-cell-links-container";
+
+export const metadata = {
+  alternates: {
+    types: {
+      "text/markdown":
+        "/oceano-academy/materiais-didaticos/panorama-biblico-at.md",
+    },
+  },
+};
+
+export default function PanoramaBiblicoAT() {
+  return (
+    <section className="relative backdrop-blur-sm">
+      <AllTeachingMaterialsBackButton tab="antigo" />
+
+      <HeroContainer className="pb-2 sm:pb-0">
+        <div className="flex flex-col gap-2">
+          <H1 className="text-left">Gênesis</H1>
+          <P className="mt-0">Deus cria o mundo e o homem.</P>
+        </div>
+      </HeroContainer>
+
+      <Separator className="my-8" />
+
+      <Container className="mb-10 sm:mb-16">
+        <H2 id="materials-didactic">Materiais de Apoio</H2>
+
+        <Table>
+          <TableHeader>
+            <TableRow className="hover:bg-transparent">
+              <TableCell className="border font-semibold w-[110px] sm:w-[160px]">
+                Apostila
+              </TableCell>
+              <TableCell>
+                <TableCellLinksContainer>
+                  <LinkSmall href="https://drive.google.com/file/d/13wjCNrB3yEZ_k75ChFCAD917J_Gr0IXW/view?usp=sharing">
+                    <FileText className="size-4" />
+                    PDF
+                  </LinkSmall>
+                </TableCellLinksContainer>
+              </TableCell>
+            </TableRow>
+
+            <TableRow className="hover:bg-transparent">
+              <TableCell className="border font-semibold">
+                Curso Recomendado
+              </TableCell>
+              <TableCell>
+                <TableCellLinksContainer>
+                  <LinkSmall href="/oceano-academy/cursos/fundamentos">
+                    <GraduationCap className="size-4" />
+                    Fundamentos
+                  </LinkSmall>
+                </TableCellLinksContainer>
+              </TableCell>
+            </TableRow>
+          </TableHeader>
+        </Table>
+      </Container>
+    </section>
+  );
+}
