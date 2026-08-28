933065ce1953e8207f3ab2768ad72ef009815921
oceanodg <65932805+oceanodg@users.noreply.github.com>
2026-08-28 04:47:19 -0300

    chore: update version

--- app/version/page.tsx
+++ app/version/page.tsx
@@ -4,7 +4,7 @@ export default function VersionPage() {
       <div className="text-center space-y-4 p-8 rounded-lg shadow-lg bg-card text-card-foreground w-96">
         <h1 className="text-2xl font-bold">Versão</h1>
         <p className="font-bold">0.1</p>
-        <p className="text-muted-foreground">23/08/2026 12:02</p>
+        <p className="text-muted-foreground">28/08/2026 04:47</p>
         <p className="text-muted-foreground">.</p>
       </div>
     </div>
